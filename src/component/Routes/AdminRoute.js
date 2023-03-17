34d6b99e24a47194cb40d0fd5eb573793f3e8937
kompargroup <89548718+kompargroup@users.noreply.github.com>
2023-03-17 16:16:53 +0100

    auth

--- src/component/Routes/AdminRoute.js
+++ src/component/Routes/AdminRoute.js
@@ -1,77 +1,29 @@
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
-import CircularProgress from '@mui/material/CircularProgress';
 import { currentAdmin } from '../../functions/auth';
-import logo from '../../images/logo/logo.png';
-import { MainContainer } from '../AppBar/Style';
-import { Typography } from '@mui/material';
 
 const AdminRoute = () => {
-  const { user, drawer } = useSelector((state) => ({ ...state }));
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [authorized, setAuthorized] = useState(false);
+  const { user } = useSelector((state) => ({ ...state }));
 
+  const [ok, setOk] = useState(false);
   useEffect(() => {
     if (user && user.token) {
       currentAdmin(user.token)
         .then((res) => {
-          // console.log(res);
-          setAuthorized(true);
+          console.log(res);
+          setOk(true);
         })
         .catch((err) => {
           console.log(err);
-          setError(true);
-        })
-        .finally(() => {
-          setLoading(false);
+          setOk(false);
         });
-    } else {
-      setLoading(false);
     }
   }, [user]);
 
-  if (loading) {
-    return (
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}
-      >
-        <CircularProgress />
-      </Box>
-    );
-  }
+  // const location = useLocation();
 
-  if (error || !authorized) {
-    return (
-      <MainContainer open={drawer}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-          }}
-        >
-          <img src={logo} alt="Kompar logo" />
-          <Box sx={{ m: 2 }}>
-            <Typography variant="body1">
-              Sorry, you are not authorized to access this resource.
-            </Typography>
-          </Box>
-        </Box>
-      </MainContainer>
-    );
-  }
-
-  return <Outlet />;
+  return ok ? <Outlet /> : <h1>you are not authorozied</h1>;
 };
 
 export default AdminRoute;
--- src/component/Routes/Missing.js
+++ src/component/Routes/Missing.js
@@ -1,31 +1,7 @@
-import { Box, Typography } from '@mui/material';
 import React from 'react';
-import { MainContainer } from '../AppBar/Style';
-import logo from '../../images/logo/logo.png';
-import { useSelector } from 'react-redux';
 
 const Missing = () => {
-  const { drawer } = useSelector((state) => ({ ...state }));
-  return (
-    <MainContainer open={drawer}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}
-      >
-        <img src={logo} alt="Kompar logo" />
-        <Box sx={{ m: 2 }}>
-          <Typography variant="body1">
-            Sorry, you are not authorized to access this resource.
-          </Typography>
-        </Box>
-      </Box>
-    </MainContainer>
-  );
+  return <div>ce lien n'est existe pas</div>;
 };
 
 export default Missing;
